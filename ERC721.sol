aed5435193ee1108fbdc729e1526954bcbe14a1a
Artem Abdullin <artemleelooai@gmail.com>
2022-08-04 22:43:10 +0300

    Added function safeTransferFrom

--- ERC721.sol
+++ ERC721.sol
@@ -5,3 +5,28 @@ interface IERC165 {
     function supportsInterface(bytes4 interfaceID) external view returns (bool);
 }
 
+interface IERC721 is IERC165 {
+    function balanceOf(address owner) external view returns (uint balance);
+
+    function ownerOf(uint tokenId) external view returns (address owner);
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId,
+        bytes calldata data
+    ) external;
+
+    function TransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+}
+
